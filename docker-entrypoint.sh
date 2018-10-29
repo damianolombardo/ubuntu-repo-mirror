8f20c0736cb2a4563a8e94b0c11250f7284e1058
Damiano Lombardo <lombardo.damiano@gmail.com>
2018-10-29 19:40:22 +0800

    removed sudo calls from docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo mkdir -p /var/spool/apt-mirror/mirror/archive.ubuntu.com/
+mkdir -p /var/spool/apt-mirror/mirror/archive.ubuntu.com/
 
-sudo mount --bind /var/spool/apt-mirror/mirror/archive.ubuntu.com/  /srv/ftp/
+mount --bind /var/spool/apt-mirror/mirror/archive.ubuntu.com/  /srv/ftp/
 
-sudo update-rc.d proftpd enable
+update-rc.d proftpd enable
 
-sudo apt-mirror
+apt-mirror
