fec565c9be2ae2fb0e09712b2ba9b5fba3113f4f
Damiano Lombardo <lombardo.damiano@gmail.com>
2018-11-24 00:31:55 +0800

    change the default proftp directory rather than mount

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,9 @@
 
 mkdir -p /var/spool/apt-mirror/mirror/archive.ubuntu.com/
 
-mount --bind /var/spool/apt-mirror/mirror/archive.ubuntu.com/  /srv/ftp/
-
 update-rc.d proftpd enable
 
 apt-mirror
+
+proftpd -f
+
